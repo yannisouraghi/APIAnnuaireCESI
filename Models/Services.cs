4085d5c8e6b7ed0e354b9f1e12110c6d32fc3b5b
Ouraghi Yannis <85176113+yannisouraghi@users.noreply.github.com>
2023-12-04 13:32:00 +0100

    Add files via upload

---
+++ Models/Services.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIAnnuaire.Models
+{
+    public class Services
+    {
+        [Key]
+        public int ServiceId { get; set; }
+        public string? Service { get; set; }
+    }
+}
---
+++ Models/Sites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIAnnuaire.Models
+{
+    public class Sites
+    {
+        [Key]
+        public int SiteId { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public string? Service { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Country { get; set; }
+    }
+}
